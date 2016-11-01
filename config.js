a4e2ca3715b92d17a128fbc1b9033c0a455ff41a
Sergio Álvarez <sergio.alvarez@mediasq.com>
2016-11-01 11:28:38 +0100

    Fix installation for postcss-hover-prefix

--- config.js
+++ config.js
@@ -17,15 +17,16 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "core-js": "npm:core-js@1.2.6",
     "fs": "github:jspm/nodelibs-fs@0.1.2",
-    "postcss": "npm:postcss@5.0.14",
+    "postcss": "npm:postcss@5.2.5",
     "postcss-calc": "npm:postcss-calc@5.2.0",
     "postcss-custom-media": "npm:postcss-custom-media@5.0.0",
+    "postcss-hover-prefix": "npm:postcss-hover-prefix@0.0.1",
     "postcss-import": "npm:postcss-import@7.1.3",
     "postcss-nested": "npm:postcss-nested@1.0.0",
     "postcss-sassy-mixins": "npm:postcss-sassy-mixins@2.0.0",
     "postcss-simple-vars": "npm:postcss-simple-vars@1.2.0",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
+      "assert": "npm:assert@1.4.1"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
@@ -37,12 +38,18 @@ System.config({
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
+      "process": "npm:process@0.11.9"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:assert@1.3.0": {
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
+    "npm:assert@1.4.1": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.35": {
@@ -56,9 +63,17 @@ System.config({
       "base64-js": "npm:base64-js@0.0.8",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.6",
+      "ieee754": "npm:ieee754@1.1.8",
       "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:chalk@1.1.3": {
+      "ansi-styles": "npm:ansi-styles@2.2.1",
+      "escape-string-regexp": "npm:escape-string-regexp@1.0.5",
+      "has-ansi": "npm:has-ansi@2.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "strip-ansi": "npm:strip-ansi@3.0.1",
+      "supports-color": "npm:supports-color@2.0.0"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -92,6 +107,9 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:has-ansi@2.0.0": {
+      "ansi-regex": "npm:ansi-regex@2.0.0"
+    },
     "npm:has-flag@1.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
@@ -120,47 +138,54 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:postcss-calc@5.2.0": {
-      "postcss": "npm:postcss@5.0.14",
+      "postcss": "npm:postcss@5.2.5",
       "postcss-message-helpers": "npm:postcss-message-helpers@2.0.0",
       "reduce-css-calc": "npm:reduce-css-calc@1.2.0"
     },
     "npm:postcss-custom-media@5.0.0": {
-      "postcss": "npm:postcss@5.0.14"
+      "postcss": "npm:postcss@5.2.5"
+    },
+    "npm:postcss-hover-prefix@0.0.1": {
+      "postcss": "npm:postcss@5.2.5"
     },
     "npm:postcss-import@7.1.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "glob": "npm:glob@5.0.15",
       "object-assign": "npm:object-assign@4.0.1",
       "path": "github:jspm/nodelibs-path@0.1.0",
-      "postcss": "npm:postcss@5.0.14",
+      "postcss": "npm:postcss@5.2.5",
       "postcss-message-helpers": "npm:postcss-message-helpers@2.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "resolve": "npm:resolve@1.1.6"
     },
     "npm:postcss-nested@1.0.0": {
-      "postcss": "npm:postcss@5.0.14",
+      "postcss": "npm:postcss@5.2.5",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:postcss-sassy-mixins@2.0.0": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "glob": "npm:glob@6.0.4",
       "path": "github:jspm/nodelibs-path@0.1.0",
-      "postcss": "npm:postcss@5.0.14",
+      "postcss": "npm:postcss@5.2.5",
       "postcss-simple-vars": "npm:postcss-simple-vars@1.2.0"
     },
     "npm:postcss-simple-vars@1.2.0": {
-      "postcss": "npm:postcss@5.0.14"
+      "postcss": "npm:postcss@5.2.5"
     },
-    "npm:postcss@5.0.14": {
+    "npm:postcss@5.2.5": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "chalk": "npm:chalk@1.1.3",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "js-base64": "npm:js-base64@2.1.9",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
-      "source-map": "npm:source-map@0.5.3",
+      "source-map": "npm:source-map@0.5.6",
       "supports-color": "npm:supports-color@3.1.2"
     },
-    "npm:process@0.11.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    "npm:process@0.11.9": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
     "npm:reduce-css-calc@1.2.0": {
       "balanced-match": "npm:balanced-match@0.1.0",
@@ -175,7 +200,13 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:source-map@0.5.3": {
+    "npm:source-map@0.5.6": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:strip-ansi@3.0.1": {
+      "ansi-regex": "npm:ansi-regex@2.0.0"
+    },
+    "npm:supports-color@2.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:supports-color@3.1.2": {
@@ -185,6 +216,9 @@ System.config({
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
